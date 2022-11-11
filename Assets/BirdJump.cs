ee0b75e6c61df4234b9eb506ffa19fba0fdf4f0a
byungsang <jo_bs@promartians.com>
2022-11-11 11:17:19 +0900

    init Commit

---
+++ Assets/BirdJump.cs
@@ -0,0 +1,20 @@
+using System.Reflection.PortableExecutable;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Debug.Log("ddddd1");;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Debug.Log("ddddd2");;
+    }
+}
